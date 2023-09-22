2cd3150c32ea319682bb3865c43630276d322111
MrIntern <nicho.santo3@gmail.com>
2023-09-22 11:48:32 +0100

    add docstrings

--- app.py
+++ app.py
@@ -2,17 +2,40 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# hello endpoint
 @app.route('/api/hello', methods=['GET'])
 def hello():
+    """
+    Returns:
+    JSON response that looks like this:
+    {
+        "message": "Hello, World!",
+        "random": "<class 'float'>"
+    }
+    """
     return jsonify(message="Hello, World!", random=str(type(123.123)))
 
-"""
-expects a POST request containing json that looks like this:
-{"numbers": [5, 3, 2, 10]}
-"""
+
 @app.route('/api/add', methods=['POST'])
 def add_numbers():
+    """
+    Add numbers provided in a JSON list.
+
+    Expects a JSON payload that looks like this:
+    {
+        "numbers": [num1, num2, ...]
+    }
+
+    Returns a JSON response with the result of adding all the numbers together.
+
+    Example:
+    - Input: {"numbers": [5, 3, 2, 10]}
+    - Output: {"result": 20}
+
+    If the input is invalid or contains fewer than two numbers, it returns an error response.
+
+    Returns:
+    - JSON response with the result or error message.
+    """
     data = request.get_json()
 
     if "numbers" in data and isinstance(data['numbers'], list):
